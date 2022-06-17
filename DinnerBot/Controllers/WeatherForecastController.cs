a8533d10cf1331842eef76221cba2facd7233af6
Владислав Дериглазов <kredis31@gmail.com>
2022-06-17 16:21:32 +0300

    Add project files.

---
+++ DinnerBot/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+
+namespace DinnerBot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("TOKEN");
+
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ DinnerBot/Program.cs
@@ -0,0 +1,66 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ITelegramBotClient bot = new TelegramBotClient("1832888971:AAHJKnVnmOgjC2TrexNVWDO59Iz77MJJLLg");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+var receiverOptions = new ReceiverOptions
+{ 
+    AllowedUpdates = { }, // receive all update types
+};
+bot.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken
+);
+
+app.Run();
+
+ static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Некоторые действия
+    Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+    if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+    {
+        var message = update.Message;
+        if (message.Text.ToLower() == "/start")
+        {
+            await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+            return;
+        }
+        await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+    }
+}
+
+ static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    // Некоторые действия
+    Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+}
